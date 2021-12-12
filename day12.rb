2f05def751872309364c13c80ea7413db20d755c
Paradise Wright <alexparadisewright+git@gmail.com>
2021-12-12 21:50:29 +0100

    Day12 - Part2 - Ruby

--- day12.rb
+++ day12.rb
@@ -8,49 +8,34 @@ nodes = {}
 
 File.foreach("day12.in") { |line| 
     edge = line.strip().split("-")
-    if nodes[edge[0]] then
-        nodes[edge[0]].append(edge[1]) 
-    else
-        nodes[edge[0]] = [edge[1]]
-    end
-
-    if nodes[edge[1]] then
-        nodes[edge[1]].append(edge[0]) 
-    else
-        nodes[edge[1]] = [edge[0]]
-    end
+    nodes[edge[0]] ? nodes[edge[0]].append(edge[1]) : nodes[edge[0]] = [edge[1]]
+    nodes[edge[1]] ? nodes[edge[1]].append(edge[0]) : nodes[edge[1]] = [edge[0]]
 }
 
 totalPaths = 0
-stack = [["start", Array.new]]
+stack = [["start", [], false]]
 while not stack.empty?
-    # puts "stack: #{stack}"
     nxt = stack.pop
-    # puts "next: #{nxt}"
+    
     node = nxt[0]
-    if node == "end" then
-        totalPaths += 1
-        next
-    end
-
     visited = nxt[1].dup
-    visited.append(node)
-    # puts "seen: #{visited}"
+    duped = nxt[2]
 
+    visited.append(node)
     edges = nodes[node] || []
-    # puts "edges: #{edges} from #{nodes}"
+
     for n in edges
+        next if n == "start"
         if n == "end" then
-            # puts "path! #{visited}"
             totalPaths += 1
             next
         end
-        if n.upper? or not visited.include?(n)
-            # puts "adding #{[n, visited]}"
-            stack.append([n, visited])
+        if n.upper? 
+            stack.append([n, visited, duped])
+        elsif not duped or not visited.include?(n)
+            stack.append([n, visited, (duped or visited.include?(n))])
         end
     end
-    # puts "------------"
 end
 
 puts totalPaths
